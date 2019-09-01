8a50fd93d49c21c712134236c63a27e63e7fe718
Jing Luo <luojing1211@gmail.com>
2019-08-31 20:40:05 -0400

    rename table dir and files in the dir

---
+++ adx/adx_table/json_adx_table.py
@@ -0,0 +1,82 @@
+"""json_adx.py
+simple json ADXTable implementation
+"""
+from .adx_table import ADXTableBase
+import json
+import os
+
+class JsonAdxTable(ADXTableBase):
+    """Simple Json table
+    """
+    def __init__(self, name, fp=None):
+        if os.path.isfile(str(fp)):
+            self.loadfile(fp)
+        elif os.path.isdir(str(fp)):
+            # look for .adx subdirectory
+            if not os.path.isdir(os.path.join(fp, '.adx')):
+                os.makedirs(os.path.join(fp, '.adx'))
+            fp = os.path.join(fp, '.adx', str(name)+'.adx.json')
+            self.loadfile(fp)
+        else:
+            self.data = dict()
+        super(JsonTable, self).__init__(name, 'JSON', tablePath=fp)
+
+
+    def close(self):
+        """close all open files
+        """
+        pass
+
+    def update(self, newValues):
+        """Update data record.
+
+        Parameters
+        ----------
+        newValues : dict
+            fields to be updated with new values
+        """
+        if not isinstance(newValues, dict):
+            raise TypeError("expected new values as a dictionary")
+
+        for k,v in newValues.iteritems():
+            self.data[k] = v
+
+        self.save()
+
+    def loadfile(self, fp=None):
+        """load ADXTable json file into memory
+        If file does not exist then load empty dictionary.
+
+        Parameters
+        ----------
+        fp : str, optional
+            filepath to table file to be loaded
+        """
+
+        if fp is None:
+            fp = self.tablepath
+
+        if os.path.isfile(fp):
+            with open(fp, 'r') as f:
+                self.data = json.load(f)
+        else:
+            self.tablepath = fp
+            self.data = json.loads('{}')
+
+    def save(self):
+        """save internal data to disk
+        """
+
+        with open(self.tablepath, 'w') as fp:
+            json.dump(self.data, fp)
+
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, datum):
+        if not isinstance(datum, dict):
+            raise TypeError("expected dictionary")
+        self._data = datum
---
+++ adx/adx_table/sqlite_adx_table.py
@@ -0,0 +1,155 @@
+"""sqliteTable.py
+sqlite3 ADXTable implementation
+"""
+
+from .adx_table import AdxTable
+import sqlite as sql
+import os
+
+class SqliteAdxTable(AdxTableBase):
+    """SQLite3 DB handler
+
+    basic implementation of an ADX Table using sqlite3 as a
+    backend.
+
+    Parameters
+    ----------
+    fp : str
+        Filepath to sqlite db file.
+    name : str, optional
+        Name of ADXTable implementation.
+
+    """
+    def __init__(self, fp=None, name='SqliteTable'):
+        super(SqliteTable, self).__init__(name, tablepath=fp)
+        self.tableMap = None
+        self.sqlConn = None
+        self.sqlCursor = None
+
+    @property
+    def sqlConn(self):
+        return self._sqlConn
+
+    @property
+    def sqlCursor(self):
+        return self._sqlCursor
+
+    @property
+    def tableMap(self):
+        return self._tableMap
+
+    @tableMap.setter
+    def tableMap(self, tmap):
+        """Set sqlite table map
+
+        Parameters
+        ----------
+        tmap : tuple
+            tuple defining table fields and their data types
+
+        Notes
+        -----
+        The table map, `tmap`, should be be a tuple of field definition
+        tuples. A field definition tuple should contain 2 elements
+        in following structure `(field_name, field_dtype)`. For example,
+        the table map for an ADXTable containing two fields could look like
+        the following: (('date', 'text'), ('avg', 'real')).
+        """
+
+        self._tableMap = tmap
+
+    def load(self, loadpath=None, create=False):
+        """load sqlite db file as ADXTable
+
+        Parameters
+        ----------
+        loadpath : str, optional
+            Path to db file
+        create : boo, optional
+            If true then create new table if `loadpath` does not exist
+            and `tableMap` has been set.
+        """
+
+        if loadpath is None:
+            loadpath = self.tablepath
+
+        if not os.path.isfile(loadpath):
+            if create:
+                # attempt to create this db file
+                if self.tableMap:
+                    table_fields = self.parse_field_list(self.tableMap)
+                    sql_cmd = "CREATE TABLE adx ({})".format(table_fields)
+                    self.db_connect(loadpath)
+                    self.sqlCursor.execute(sql_cmd)
+                    self.sqlConn.commit()
+                else:
+                    emsg = "cannot create new Sqlite ADX Table without \
+                            a tablemap"
+                    raise RuntimeError(emsg)
+            else:
+                emsg = "no such file: {}".format(loadpath)
+                raise IOError(emsg)
+        else:
+            self.db_connect(loadpath)
+
+    def db_connect(self, fp):
+        """initiate sqlite3 db connection
+        Set `self.sqlConn` and `self.sqlCursor` with sqlite3
+        connection parameters.
+
+        Parameters
+        ----------
+        fp : str
+            File path to sqlite3 db file
+        """
+        conn = sql.connect(fp)
+        cur = conn.cursor()
+
+        self._sqlConn = conn
+        self._sqlCursor = cur
+
+    @staticmethod
+    def parse_field_list(tmap):
+        """parse table map field list
+
+        Parameters
+        ----------
+        tmap : tuple
+            tuple defining fieldnames and data types for sqlite db
+
+        Returns
+        -------
+        tmap_str : str
+            field value pairs as a string that can be used to
+            create a table as an sql query
+        """
+
+        s = ''
+        N = len(tmap)
+        for i in range(N):
+            if i:
+                s += ', '
+            f,v = tmap[i]
+            s += "{} {}".format(f, v)
+        return s
+
+    def close(self):
+        """close the db file
+        """
+        self.sqlConn.close()
+
+    def update(self, newResult):
+        """Update sqlite table
+
+        Parameters
+        ----------
+        newResult : tuple
+            results to be updated to latest record in database.
+
+        Notes
+        -----
+            `newResult` must be a tuple of (field, value) pairs
+        """
+        fields = self.parse_field_list(newResult)
+        query = "UPDATE adx SET "
+        pass
