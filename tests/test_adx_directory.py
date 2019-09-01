6b869167d967ef28f9c41e9ab079a9f0bf573e25
Jing Luo <luojing1211@gmail.com>
2019-08-31 20:41:32 -0400

    change test data name due to the renaming of data files

--- tests/test_adx_directory.py
+++ tests/test_adx_directory.py
@@ -8,29 +8,26 @@ import json
 from adx.adx_directory import *
 
 
+test_data = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                         'test_data', 'data_dir_noadx')
 
 class TestInit:
 
     def setup(self):
-        self.data_dir = ('./fs/lofasm1')
-        self.adx_dir = os.path.join(self.data_dir, 'adx_log')
-        try:
-            shutil.rmtree(sefl.adx_dir)
-        except:
-            pass       
+        self.data_dir = test_data
 
     def test_non_adx_dir(self):
         dd = DataDirectory(self.data_dir)
-        assert not dd.isadx 
-        
+        assert not dd.isadx
+
     def test_init(self):
         init_adx_dir(self.data_dir, ['lofasm', 'bbx'])
         config_file = os.path.join(self.data_dir, 'adx_log/config')
         assert os.path.exists(config_file)
         f = open(config_file, 'r')
         config = json.load(f)
-        shutil.rmtree(self.adx_dir)
-   
+        shutil.rmtree(adx_dir)
+
     def test_adx_dir(self):
         init_adx_dir(self.data_dir, ['lofasm', 'bbx'])
         dd = DataDirectory(self.data_dir)
