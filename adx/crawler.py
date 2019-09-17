f11f2ef7a8a497cf8f322de799ea104a9e79c4af
Jing Luo <luojing1211@gmail.com>
2019-09-17 15:16:21 -0400

    make crawler returen as a dict

--- adx/crawler.py
+++ adx/crawler.py
@@ -39,12 +39,12 @@ class Crawler:
 
     def dfs(self):
         visited, stack = set(), [self.root_dir]
-        result = []
+        result = {}
         while stack:
             cur = stack.pop()
             if cur not in visited:
                 visited.add(cur)
                 cur_info = DataDirectory(cur)
-                result.append((cur, cur_info))
+                result.update({cur: cur_info})
                 stack.extend(set(cur_info.next) - visited)
         return result
